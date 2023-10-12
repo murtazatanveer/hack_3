f2eb3aa528a8cda8bc4f051073d4a267dd820049
murtazatanveer <murtaza.tanveer488@gmail.com>
2023-10-13 03:11:36 +0500

    19th

--- datausage.c
+++ datausage.c
@@ -59,7 +59,7 @@ else if(g>=avg){
   	
 float l=rd/j;
   printf("\n\n.............A L E R T...............\n\n");
-  	printf(" You are at or below your average daily use of %.2f GBs Per Day.\n\n You can use up to %.2f GBs Per Day and stay below your data plan limit.\n\n",g,l)  
+  	
     
     }
 
