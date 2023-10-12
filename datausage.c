daea03fe7694088f4fab0c41344c497c4505f858
murtazatanveer <murtaza.tanveer488@gmail.com>
2023-10-13 03:01:31 +0500

    2ND

--- datausage.c
+++ datausage.c
@@ -2,6 +2,12 @@
 int main(){
 
 
+// Made by : Murtaza Tanveer
+//Reg No   : SP23-BSE-096-2C
+
+
+    printf("\nMade by : MURTAZA TANVEER\nReg no  : SP23-BSE-096-2C");
+	printf("\n\n____________________WELCOME TO MOBILE DATA USAGE TRACKER___________________\n\n\n");
 
 
 
@@ -24,5 +30,4 @@ int main(){
 
 
 
-    
 }
