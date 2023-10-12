332d0aa0dcb34426cf777318250bb629652e17f8
murtazatanveer <murtaza.tanveer488@gmail.com>
2023-10-13 03:02:38 +0500

    3RD

--- datausage.c
+++ datausage.c
@@ -6,10 +6,12 @@ int main(){
 //Reg No   : SP23-BSE-096-2C
 
 
-    printf("\nMade by : MURTAZA TANVEER\nReg no  : SP23-BSE-096-2C");
-	printf("\n\n____________________WELCOME TO MOBILE DATA USAGE TRACKER___________________\n\n\n");
+printf("\nMade by : MURTAZA TANVEER\nReg no  : SP23-BSE-096-2C");
+printf("\n\n____________________WELCOME TO MOBILE DATA USAGE TRACKER___________________\n\n\n");
 
-
+float a;
+printf("\n\n Enter number of GBs in the plan per 30 day period : ");
+scanf("%f",&a);
 
 
 
